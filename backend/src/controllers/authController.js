da4bd4037b9358b7d4790c9646a6ab86f81632c7
ShivamRai1212 <shivam@rssrsoftwares.com>
2026-09-25 11:50:54 +0530

    update

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -7,7 +7,7 @@ const generateToken = (id) => {
   });
 };
 
-// @desc    Phone Number Login / Registrations Data welcome
+// @desc    Phone Number Login / Registrations Data
 // @route   POST /api/auth/phone-login
 exports.phoneLogin = async (req, res) => {
   try {
